215fba554652b0becc20b003e631142b8d5219d1
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-16 11:32:16 -0300

    Add FundTransferContext

---
+++ src/FundTransfer.Infra/Data/FundTransferContext.cs
@@ -0,0 +1,15 @@
+using FundTransfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FundTransfer.Infra.Data
+{
+    public class FundTransferContext : DbContext
+    {
+        public FundTransferContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TransferOrder> TransferOrders { get; set; }
+        
+    }
+}
