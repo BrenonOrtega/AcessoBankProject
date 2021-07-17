429b90f3fabe6357b59cd815f91592af60962ac3
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-17 16:08:53 -0300

    Adds Credit and Debit Post Adjustment to BalanceInterface

--- src/FundTransfer.Domain/Repositories/Commands/IBalanceAdjustmentCommandRepository.cs
+++ src/FundTransfer.Domain/Repositories/Commands/IBalanceAdjustmentCommandRepository.cs
@@ -6,5 +6,9 @@ namespace FundTransfer.Domain.Repositories.Commands
     public interface IBalanceAdjustmentCommandRepository
     {
         Task PostAdjustment(BalanceAdjustment adjustment);
+
+        Task PostDebitdjustment(string accountNumber, decimal Value);
+
+        Task PostCreditAdjustment(string accountNumber, decimal Value);
     }
 }
