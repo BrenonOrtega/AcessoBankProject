84c1599215d938f1ebeb80705c145438cbe99d9c
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-17 16:43:12 -0300

    Fix Model validation Logic error

--- src/FundTransfer.Domain/Models/Account.cs
+++ src/FundTransfer.Domain/Models/Account.cs
@@ -1,21 +1,24 @@
 using System;
+using FundTransfer.Domain.Repositories.Queries;
 
 namespace FundTransfer.Domain.Models
 {
     public class Account
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
 
         public decimal Balance { get; set; }
 
         public string AccountNumber { get; set; }
 
-        public bool IsEmpty()
+        public bool IsValid()
         {
-            return Id.Equals(0) 
-                && Balance.Equals(decimal.Zero) 
-                && String.IsNullOrEmpty(AccountNumber)
-                ;
+            if (Id.Equals(0) && Balance.Equals(decimal.Zero) && String.IsNullOrEmpty(AccountNumber))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
