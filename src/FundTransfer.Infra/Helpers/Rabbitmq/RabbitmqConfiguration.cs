f8a257268b2dec61c275cfefca715ef0c8029311
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-17 16:46:13 -0300

    Adds RabbitmqConfig class for configuration data.

---
+++ src/FundTransfer.Infra/Helpers/Rabbitmq/RabbitmqConfiguration.cs
@@ -0,0 +1,12 @@
+namespace FundTransfer.Infra.Helpers.Rabbitmq
+{
+    public class RabbitmqConfiguration
+    {
+        public const string RabbitMQ = "rabbitmq";
+        public string Hostname { get; set; }
+        public int Port { get; set; }
+        public string QueueName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
