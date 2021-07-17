61e1e5a8fbaa96f6110d1fd275d5e591b2d49498
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-17 16:37:40 -0300

    Add EF Core Migrations

---
+++ src/FundTransfer.Application/Migrations/20210716194004_Initial Migration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundTransfer.Application.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransferOrders",
+                columns: table => new
+                {
+                    TransactionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SourceAccountNumber = table.Column<string>(type: "text", nullable: false),
+                    DestinationAccountNumber = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<decimal>(type: "numeric", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransferOrders", x => x.TransactionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransferOrders");
+        }
+    }
+}
