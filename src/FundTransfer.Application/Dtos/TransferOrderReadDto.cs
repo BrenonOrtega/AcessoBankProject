128128c46bd30eab7cf94967d8f84c16b7fd7be6
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-17 22:07:25 -0300

    Adds TransferOrderReadDto

---
+++ src/FundTransfer.Application/Dtos/TransferOrderReadDto.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using FundTransfer.Domain.Enum;
+using FundTransfer.Domain.Models;
+
+namespace FundTransfer.Application.Dtos
+{
+    public class TransferOrderReadDto
+    {
+        public Guid TransactionId { get; set; }
+
+        [EnumDataType(typeof(TransferOrderStatus))]
+        public string Status { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public static implicit operator TransferOrderReadDto(TransferOrder order)
+        {
+            return new TransferOrderReadDto() 
+            {
+                TransactionId = order.TransactionId,
+                Status = order.Status.ToString(),
+                ErrorMessage = order.ErrorMessage
+            };
+        }
+        
+    }
+}
