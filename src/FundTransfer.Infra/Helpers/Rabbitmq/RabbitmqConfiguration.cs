2469bf771f903e8ed7cb692dd20f05b74c6108ee
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-18 13:49:55 -0300

    Adds NetworkInterval property for configuration

--- src/FundTransfer.Infra/Helpers/Rabbitmq/RabbitmqConfiguration.cs
+++ src/FundTransfer.Infra/Helpers/Rabbitmq/RabbitmqConfiguration.cs
@@ -8,5 +8,6 @@ namespace FundTransfer.Infra.Helpers.Rabbitmq
         public string QueueName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public int NetworkRecoveryIntervalInSeconds { get; set; }
     }
 }
