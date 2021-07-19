c3152684c24c413743609a519c1af0708ef30340
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-19 19:52:39 -0300

    adds behavor for handling TransferOrders at DTO .

--- src/FundTransfer.Application/Dtos/TransferOrderReadDto.cs
+++ src/FundTransfer.Application/Dtos/TransferOrderReadDto.cs
@@ -7,21 +7,26 @@ namespace FundTransfer.Application.Dtos
 {
     public class TransferOrderReadDto
     {
-        public Guid TransactionId { get; set; }
+        public Guid TransactionId { get; private set; }
 
         [EnumDataType(typeof(TransferOrderStatus))]
-        public string Status { get; set; }
+        public string Status { get; init; }
 
-        public string ErrorMessage { get; set; }
+        public string ErrorMessage { get; init; }
 
-        public static implicit operator TransferOrderReadDto(TransferOrder order)
+        public TransferOrderReadDto(string errorMessage)
         {
-            return new TransferOrderReadDto() 
-            {
-                TransactionId = order.TransactionId,
-                Status = order.Status.ToString(),
-                ErrorMessage = order.ErrorMessage
-            };
+            TransactionId = Guid.Empty;
+            Status = TransferOrderStatus.Error.ToString();
+            ErrorMessage = errorMessage;
+        }
+
+        public TransferOrderReadDto(TransferOrder order)
+        {
+            TransactionId = order.TransactionId;
+            Status = order.Status.ToString();
+            ErrorMessage = order.ErrorMessage;
+
         }
     }
 }
