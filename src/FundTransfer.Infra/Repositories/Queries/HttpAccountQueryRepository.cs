6899071eb862408ac583cc83710c710f2751d11c
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-07-18 22:10:29 -0300

    Implements deserialization to http account repo.

--- src/FundTransfer.Infra/Repositories/Queries/HttpAccountQueryRepository.cs
+++ src/FundTransfer.Infra/Repositories/Queries/HttpAccountQueryRepository.cs
@@ -1,19 +1,22 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using FundTransfer.Domain.Repositories.Queries;
-using FundTransfer.Domain.Models;
-using FundTransfer.Infra.Services;
 using Microsoft.Extensions.Logging;
+using FundTransfer.Domain.Models;
 using FundTransfer.Domain.Services;
+using FundTransfer.Domain.Repositories.Queries;
 
 namespace FundTransfer.Infra.Repositories.Queries
 {
     public class HttpAccountQueryRepository : IAccountQueryRepository
     {
-        private readonly IAccountService _service;
+        private readonly IAccountService<HttpResponseMessage> _service;
         public ILogger _logger;
         
-        public HttpAccountQueryRepository(IAccountService service, ILogger<HttpAccountQueryRepository> logger)
+        public HttpAccountQueryRepository(IAccountService<HttpResponseMessage> service, ILogger<HttpAccountQueryRepository> logger)
         {
             _service = service;
             _logger = logger;
@@ -21,17 +24,26 @@ namespace FundTransfer.Infra.Repositories.Queries
 
         public async Task<IEnumerable<Account>> GetAll()
         {
-            var accounts = await _service.GetAccounts();
+            var response = await _service.GetAccounts();
+            var content = await response.Content.ReadAsByteArrayAsync();
+            var encodedContent = Encoding.UTF8.GetString(content);
+            var accounts = JsonSerializer.Deserialize<IEnumerable<Account>>(encodedContent, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+
             return accounts;
         }
 
         public async Task<Account> GetByAccountNumber(string accountNumber)
         {
-            var account = await _service.GetByAccountNumber(accountNumber);
+            var response = await _service.GetByAccountNumber(accountNumber);
+            
+            if (response.StatusCode.Equals(HttpStatusCode.NotFound))
+                return new Account();
+            
+            var content = await response.Content.ReadAsByteArrayAsync();
+            var encodedContent = Encoding.UTF8.GetString(content);
+            var account = JsonSerializer.Deserialize<Account>(encodedContent, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+
             return account;
         }
-
-       
     } 
-    
 }
